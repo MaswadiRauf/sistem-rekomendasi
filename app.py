c02bb1ddc8bbc2700173588e90f4e7e1d73b57b0
Maswadi Rauf <maswadirauf07@gmail.com>
2026-07-14 11:22:45 +0700

    vercel-1

--- app.py
+++ app.py
@@ -886,7 +886,9 @@ def metrics():
     )
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
-    )
+    app.debug = False
+    
+    
+# kalo mau lokal
+# if __name__ == "__main__":
+#     app.run(debug=True)
